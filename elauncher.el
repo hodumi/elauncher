a5b4c68d8c645b28168f118309f6729084021e16
hodumi <ajsmithy00@gmail.com>
2026-08-09 16:42:23 +0900

    add program

---
+++ elauncher.el
@@ -0,0 +1,54 @@
+;;; elauncher.el --- a                               -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2026  
+
+;; Author: Yusuke KUDO <ajsmithy00@gmail.com>
+;; Keywords: tools
+;; URL: https://github.com/hodumi/elauncher
+;; Version: 0.1
+
+;;; Commentary:
+
+;; EmacsからWindows上のプログラムを起動する関数を定義するパッケージです。
+;; 定義した関数は、M-xやキーバインドから呼び出せます。
+;;
+;; (elauncher:defapplication 関数名 プログラムパス パラメータ)
+;;  プログラムパスのプログラムを起動します。
+
+;; (elauncher:defexplorer 関数名 フォルダパス)
+;;  エクスプローラでフォルダパスを開きます。
+
+;; Examples.
+;;(elauncher:defexplorer elauncher:open-default-directory default-directory) ; default-directoryの表示
+;;(elauncher:defexplorer elauncher:open-home-directory "~" ) ; HOMEの表示
+;;(elauncher:defapplication run-calc "calc") ; calc.exeの起動
+
+
+;;; Code:
+
+(defun elauncher:open-directory (&optional directory)
+   (when (string-equal system-type "windows-nt")
+     (let ((dir (or directory default-directory)))
+       (message "Opend: %s" dir)
+       (w32-shell-execute "explore" dir))))
+
+(defun elauncher:run-application (file &optional parameters)
+  (when (string-equal system-type "windows-nt")
+    (message "Opend: %s %s" file parameters)
+    (w32-shell-execute "open" file parameters)))   
+
+(defmacro elauncher:defexplorer (name directory)
+  `(defun ,name ()
+     (interactive)
+     (elauncher:open-directory ,directory)))
+
+(defmacro elauncher:defapplication (name file &optional parameters)
+  `(defun ,name ()
+     (interactive)
+     (elauncher:run-application ,file ,parameters)))
+
+
+
+(provide 'elauncher)
+;;; elauncher ends here
+
